b0a86add254bf5f7522c60e7c622e6c60ff179f3
Frieden <amanmawar17141@gmail.com>
2023-02-05 14:00:24 +0530

    resolved unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
+import { ConnectComponent } from './connect/connect.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from './project/project.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,7 +14,12 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        AboutComponent,
+        ConnectComponent,
+        ExperienceComponent,
+        ProjectComponent,
       ],
     }).compileComponents();
   });
@@ -24,12 +34,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('portfolio');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('portfolio app is running!');
   });
 });
