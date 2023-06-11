a55df5d8cfbb2057197ace62810f0e83c43ef9b3
Frieden <amanmawar17141@gmail.com>
2023-06-11 17:46:26 +0530

    removed unused packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,50 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    themes: [
-      {
-        screens: {
-          sm: "480px",
-          md: "768px",
-          lg: "976px",
-          xl: "1440px",
-        },
-        colors: {
-          primary: "#242219",
-          
-          secondary: "#FBBD23",
-
-          white: "#FFFFFF",
-
-          black: "#000000",
-
-          info: "#3ABFF8",
-
-          success: "#36D399",
-
-          warning: "#FBBD23",
-
-          error: "#F87272",
-        },
-        fontfamily:{
-        }
-      },
-    ],
-  content: ["./src/**/*.{html,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require("@tailwindcss/typography"), 
-    require("daisyui")
-  ],
-  daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dark",
-  },
+export const content = [
+  "./src/**/*.{html,ts}",
+];
+export const theme = {
+  extend: {},
 };
+export const plugins = [];
+
